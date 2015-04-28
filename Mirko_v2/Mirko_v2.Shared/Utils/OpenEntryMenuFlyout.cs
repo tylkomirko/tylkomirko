8c0efcb7d1c84e66ce8334bfcef50de0582758ca
LubieLiscie <tylkomirko@hotmail.com>
2015-04-28 09:17:57 +0200

    Further tweaks.

--- Mirko_v2/Mirko_v2.Shared/Utils/OpenEntryMenuFlyout.cs
+++ Mirko_v2/Mirko_v2.Shared/Utils/OpenEntryMenuFlyout.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xaml.Interactivity;
+using Mirko_v2.ViewModel;
 using System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -14,92 +15,57 @@ namespace Mirko_v2.Utils
             var src = sender as Controls.Entry;
             FlyoutBase flyoutBase = FlyoutBase.GetAttachedFlyout(src.GetDescendant<Grid>("EntryGrid"));
 
-            var entry = src.DataContext as Entry;
-            var comment = src.DataContext as EntryComment;
+            var entryData = (src.DataContext as EntryViewModel).EntryData;
 
             var userInfo = App.ApiService.UserInfo;
             if (userInfo == null || userInfo.UserName == null)
                 return null;
 
-            if (userInfo != null && userInfo.UserName != null)
+            var myUserName = userInfo.UserName;
+
+            Action<string> makeFlyoutItemVisible = (name) =>
             {
-                var myUserName = userInfo.UserName;
-
-                Action<string> makeFlyoutItemVisible = (name) =>
+                var items = (flyoutBase as MenuFlyout).Items;
+                foreach (var i in items)
                 {
-                    var items = (flyoutBase as MenuFlyout).Items;
-                    foreach (var i in items)
+                    if (i.Tag != null && i.Tag.ToString() == name)
                     {
-                        if (i.Tag != null && i.Tag.ToString() == name)
-                        {
-                            i.Visibility = Visibility.Visible;
-                            break;
-                        }
-                    }
-                };
-
-                Action<string> makeFlyoutItemInvisible = (name) =>
-                {
-                    var items = (flyoutBase as MenuFlyout).Items;
-                    foreach (var i in items)
-                    {
-                        if (i.Tag != null && i.Tag.ToString() == name)
-                        {
-                            i.Visibility = Visibility.Collapsed;
-                            break;
-                        }
-                    }
-                };
-
-                /*
-                if (src.RootEntry != null)
-                {
-                    if (comment != null && comment.author == myUserName && !comment.deleted)
-                    {
-                        makeFlyoutItemVisible("delete");
-                        makeFlyoutItemInvisible("plus");
-                        makeFlyoutItemInvisible("favourite");
-
-                        var time = DateTime.Parse(comment.date, null, System.Globalization.DateTimeStyles.RoundtripKind);
-                        if(DateTime.Now - time < new TimeSpan(0, 10, 0))
-                            makeFlyoutItemVisible("edit");
-                    }
-                    else if (comment != null && src.RootEntry.author == myUserName && !comment.deleted)
-                    {
-                        makeFlyoutItemVisible("delete");
-                        makeFlyoutItemInvisible("favourite");
+                        i.Visibility = Visibility.Visible;
+                        break;
                     }
                 }
-                else
+            };
+
+            Action<string> makeFlyoutItemInvisible = (name) =>
+            {
+                var items = (flyoutBase as MenuFlyout).Items;
+                foreach (var i in items)
                 {
-                    if (entry != null && !entry.deleted && entry.user_favorite)
+                    if (i.Tag != null && i.Tag.ToString() == name)
                     {
-                        makeFlyoutItemInvisible("favourite");
-                        makeFlyoutItemVisible("unfavourite");
+                        i.Visibility = Visibility.Collapsed;
+                        break;
                     }
-                    else if (entry != null && entry.author == myUserName && !entry.deleted)
-                    {
-                        makeFlyoutItemVisible("edit");
-                        makeFlyoutItemVisible("delete");
-                        makeFlyoutItemInvisible("plus");
+                }
+            };
 
-                        var time = DateTime.Parse(entry.date, null, System.Globalization.DateTimeStyles.RoundtripKind);
-                        if ((entry.comment_count == 0) && (DateTime.Now - time < new TimeSpan(0, 10, 0)))
-                            makeFlyoutItemVisible("edit");
-                    }
-                    else if (comment != null && comment.author == myUserName && !comment.deleted)
-                    {
-                        makeFlyoutItemVisible("delete");
-                        makeFlyoutItemInvisible("plus");
-                        makeFlyoutItemInvisible("favourite");
-
-                        var time = DateTime.Parse(comment.date, null, System.Globalization.DateTimeStyles.RoundtripKind);
-                        if (DateTime.Now - time < new TimeSpan(0, 10, 0))
-                            makeFlyoutItemVisible("edit");
-                    }
-                }*/
-
+            if(entryData.Voted)
+            {
+                makeFlyoutItemInvisible("plus");
+                makeFlyoutItemVisible("unplus");
             }
+
+            if (entryData.Favourite)
+            {
+                makeFlyoutItemInvisible("favourite");
+                makeFlyoutItemVisible("unfavourite");
+            }
+
+            if(entryData.AuthorName != myUserName)
+            {
+                makeFlyoutItemInvisible("separator"); // FIXME
+            }
+
             flyoutBase.ShowAt(src);
 
             return null;
