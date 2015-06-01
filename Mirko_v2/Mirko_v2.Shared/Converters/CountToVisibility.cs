fb034f201fe4b22a140229bc5b2ce6be03c741f6
LubieLiscie <tylkomirko@hotmail.com>
2015-06-01 17:39:17 +0200

    Add NoItems message for #, @ and PM.

--- Mirko_v2/Mirko_v2.Shared/Converters/CountToVisibility.cs
+++ Mirko_v2/Mirko_v2.Shared/Converters/CountToVisibility.cs
@@ -19,4 +19,20 @@ namespace Mirko.Converters
             throw new NotImplementedException();
         }
     }
+
+    public class CountToVisibilityInv : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string str)
+        {
+            if (value is uint)
+                return (uint)value > 0 ? Visibility.Collapsed : Visibility.Visible;
+            else
+                return (int)value > 0 ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string str)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
