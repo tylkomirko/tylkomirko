13c87fb8f4bf58a3559d2f03e5da4f1e619a1ecf
LubieLiscie <tylkomirko@hotmail.com>
2015-06-22 18:23:28 +0200

    Initial work on new payment model.

---
+++ Mirko_v2/Mirko_v2.Shared/ViewModel/PaymentViewModel.cs
@@ -0,0 +1,41 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Messaging;
+using MetroLog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.ApplicationModel.Store;
+
+namespace Mirko_v2.ViewModel
+{
+    public class PaymentViewModel : ViewModelBase
+    {
+        private readonly ILogger Logger = null;
+        private LicenseInformation LicenseInformation;
+
+        public PaymentViewModel()
+        {
+            Logger = LogManagerFactory.DefaultLogManager.GetLogger<PaymentViewModel>();
+            LicenseInformation = CurrentApp.LicenseInformation;
+
+            Messenger.Default.Register<NotificationMessage>(this, ReadMessage);
+        }
+
+        private async void ReadMessage(NotificationMessage obj)
+        {
+            if (obj.Notification == "Init")
+            {
+                var products = await CurrentApp.GetUnfulfilledConsumablesAsync();
+                foreach (UnfulfilledConsumable product in products)
+                {
+                    Logger.Info("UnfulfilledConsumable: Product Id: " + product.ProductId + " Transaction Id: " + product.TransactionId);
+                    var status = await CurrentApp.ReportConsumableFulfillmentAsync(product.ProductId, product.TransactionId);
+                    if (status != FulfillmentResult.Succeeded)
+                        Logger.Warn("Transaction " + product.TransactionId + " couldn't be fullfilled. Status: " + status);
+                    else
+                        Logger.Info("Transaction " + product.TransactionId + " fullfilled.");
+                }
+            }
+        }
+    }
+}
