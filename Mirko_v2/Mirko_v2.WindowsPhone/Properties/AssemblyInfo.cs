e0556810a0a46d9e7238d955e25865e3cfda6420
LubieLiscie <tylkomirko@hotmail.com>
2015-07-10 13:35:14 +0200

    New build.

--- Mirko_v2/Mirko_v2.WindowsPhone/Properties/AssemblyInfo.cs
+++ Mirko_v2/Mirko_v2.WindowsPhone/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mirko_v2.WindowsPhone")]
+[assembly: AssemblyTitle("Tylko Mirko")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mirko_v2.WindowsPhone")]
+[assembly: AssemblyProduct("Tylko Mirko")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
