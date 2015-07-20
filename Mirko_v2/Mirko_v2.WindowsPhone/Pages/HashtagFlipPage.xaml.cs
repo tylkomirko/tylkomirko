cbbf6d067ade05d2224e114082b66bb699049ecf
LubieLiscie <tylkomirko@hotmail.com>
2015-07-20 15:31:37 +0200

    Fix margins in HashtagFlipPage.

--- Mirko_v2/Mirko_v2.WindowsPhone/Pages/HashtagFlipPage.xaml.cs
+++ Mirko_v2/Mirko_v2.WindowsPhone/Pages/HashtagFlipPage.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using GalaSoft.MvvmLight.Ioc;
+using Mirko_v2.Utils;
+using Mirko_v2.ViewModel;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -19,9 +11,23 @@ namespace Mirko_v2.Pages
 {
     public sealed partial class HashtagFlipPage : UserControl
     {
+        private double HeaderHeight;
+
         public HashtagFlipPage()
         {
             this.InitializeComponent();
+
+            HeaderHeight = SimpleIoc.Default.GetInstance<MainViewModel>().ListViewHeaderHeight + 49;
+
+            FlipView.Margin = new Thickness(0, -HeaderHeight, 0, 0);
+        }
+
+        private void ListViewEx_Loaded(object sender, RoutedEventArgs e)
+        {
+            var lv = sender as ListView;
+            var sp = lv.Header as StackPanel;
+            var rect = sp.GetDescendant<Rectangle>();
+            rect.Height = HeaderHeight;
         }
     }
 }
