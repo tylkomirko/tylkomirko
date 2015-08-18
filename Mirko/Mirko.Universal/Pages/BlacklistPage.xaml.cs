6dfc620502933ae80adeac224ccd92d3236680c3
LubieLiscie <tylkomirko@hotmail.com>
2015-08-18 16:00:54 +0200

    Tweaks in BlacklistPage in UWP.

--- Mirko/Mirko.Universal/Pages/BlacklistPage.xaml.cs
+++ Mirko/Mirko.Universal/Pages/BlacklistPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Mirko.ViewModel;
+using Windows.UI.Xaml.Controls;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -6,6 +7,11 @@ namespace Mirko.Pages
 {
     public sealed partial class BlacklistPage : UserControl
     {
+        private BlacklistViewModel VM
+        {
+            get { return DataContext as BlacklistViewModel; }
+        }
+
         public BlacklistPage()
         {
             this.InitializeComponent();
