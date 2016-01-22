8c762ae7ce5424603980a051295a689dce41c760
LubieLiscie <tylkomirko@hotmail.com>
2016-01-22 22:09:40 +0100

    Implement UpdateTask.

---
+++ BackgroundTasks/UpdateTask.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Windows.ApplicationModel.Background;
+using Windows.Storage;
+
+namespace BackgroundTasks
+{
+    public sealed class UpdateTask : IBackgroundTask
+    {
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            var deferral = taskInstance.GetDeferral();
+            var tempFolder = ApplicationData.Current.TemporaryFolder;
+            var localFolder = ApplicationData.Current.LocalFolder;
+
+            try
+            {
+                var imgCacheFolder = await tempFolder.GetFolderAsync("ImageCache");
+                await imgCacheFolder.DeleteAsync(StorageDeleteOption.PermanentDelete);
+            } 
+            catch(Exception) { }
+
+            try
+            {
+                var imgCacheFolder = await localFolder.GetFolderAsync("ImageService");
+                await imgCacheFolder.DeleteAsync(StorageDeleteOption.PermanentDelete);
+            }
+            catch (Exception) { }
+
+            deferral.Complete();
+        }
+    }
+}
